322609db56561d5a2f1baceb1192320bd0a7169c
Гаврилов Григорий <GGavrilov@sberbank.ru>
2023-02-11 13:12:59 +0300

    Добавит в сборку react

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,90 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
 const path = require('path');
 
+const plugins = []
+let mode = 'development';
+let target = 'web'; // в режиме разработки browserslist не используется
+if (process.env.NODE_ENV === 'production') {
+    mode = 'production';
+    target = 'browserslist'; // в продакшен режиме используем browserslist
+}
+
+if (process.env.SERVE) { // Используем плагин только если запускаем devServer
+    plugins.push(new ReactRefreshWebpackPlugin());
+} //
+
+
 module.exports = {
-    entry: './src/index.ts',
+    mode,
+    target,
+    entry: './src/index.tsx',
     module: {
         rules: [
             {
-                test: /\.ts?$/,
+                test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/,
-            }
+            },
+            {
+                test: /\.(s[ac]|c)ss$/i, // /\.(le|c)ss$/i если вы используете less
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+                type: mode === 'production' ? 'asset' : 'asset/resource', // В продакшен режиме
+                // изображения размером до 8кб будут инлайнится в код
+                // В режиме разработки все изображения будут помещаться в dist/assets
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/, // не обрабатываем файлы из node_modules
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true, // Использование кэша для избежания рекомпиляции
+                        // при каждом запуске
+                    },
+                },
+            },
         ],
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
     },
     output: {
-        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'assets/[hash][ext][query]', // Все ассеты будут
+        // складываться в dist/assets
+        clean: true,
     },
 
     plugins: [
         new HtmlWebpackPlugin({
             title: 'our project',
-            template: 'index.html' })
+            template: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css', // Формат имени файла
+        }),
+        ...plugins
     ],
 
     devServer: {
         static: path.join(__dirname, "dist"),
         compress: true,
         port: 4000,
+        hot: true,
     },
 };
