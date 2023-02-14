0441aaec59c9d09094285838885b99791b45c620
Гаврилов Григорий <GGavrilov@sberbank.ru>
2023-02-14 23:42:51 +0300

    Добавит возможность билда нескольких проектов

--- root/src/Component/index.tsx
+++ root/src/Component/index.tsx
@@ -1,10 +1,39 @@
-import React from "react";
-import {memo} from "react";
+import React, { useEffect, useState } from "react";
+import { memo } from "react";
 
 function Component() {
-    return <div>
-            hello
-        </div>
+  const [Component, setComponent] = useState<React.FC>(null);
+
+  useEffect(() => {
+    fetch("./micro/path/import-map.json")
+      .then((res) => res.json())
+      .then((res) => {
+        // console.log(res.imports["micro/path/"]);
+
+        console.log(`/${res.imports["micro/path/"]}`);
+        // const a = React.lazy(
+        //   async () => await import(`/${res.imports["micro/path/"]}`)
+        // );
+        // setComponent(a);
+        // System.import(`/${res.imports["micro/path/"]}`).then((s) => {
+        //   setComponent(s as any);
+        // });
+        // const Add = React.lazy(
+        //
+        // );
+
+        // console.log(Add);
+        // setComponent(Add);
+      });
+  }, []);
+
+  if (!Component) return null;
+
+  return (
+    <div>
+      <Component />
+    </div>
+  );
 }
 
-export default memo(Component)
+export default memo(Component);
